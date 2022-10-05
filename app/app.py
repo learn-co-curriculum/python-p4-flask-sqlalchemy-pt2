dc0e8f0016a046f8bb421722adc78ee3d758c514
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-10-05 16:29:11 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,7 +2,21 @@
 
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from models import db
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5555)
