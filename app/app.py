b6ddf93f0c5353a578430cc98b9178046e09154e
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-10-05 16:48:42 -0400

    Update app.py
    Resolved `KeyError: <weakref at 0x10f064220; to 'Flask' at 0x10e13e0a0>` error by removing extraneous definition for db. We didn't need the SQLAlchemy import either. python shell works now to seed data!

--- app/app.py
+++ app/app.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from models import db
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
