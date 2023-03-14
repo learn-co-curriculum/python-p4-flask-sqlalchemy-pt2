c69efdaf124e341b043d1370c7863225b287cdab
Ben Botsford <ben.botsford@flatironschool.com>
2023-03-14 11:07:07 -0500

    fix unnamed constraint error

--- server/models.py
+++ server/models.py
@@ -1,6 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 class Owner(db.Model):
     __tablename__ = 'owners'
